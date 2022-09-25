14e12589329c8bb2229452fec60e39447c16f62c
Max <maxbretschneider@mailbox.org>
2022-09-25 21:00:18 +0200

    Set ORB and read images

--- image_stitching.cpp
+++ image_stitching.cpp
@@ -7,5 +7,25 @@
 using namespace std;
 
 int main() {
+    // We use FAST for keypoint detection and BRIEF for description
+    // Combined in ORB for orientation component
     cv::Ptr<cv::ORB> detector = cv::ORB::create();
+
+    cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::BRUTEFORCE);
+
+    std::vector<cv::KeyPoint> lastFrameKeypoints1, lastFrameKeypoints2;
+
+    cv::Mat lastFrameDescriptors1, lastFrameDescriptors2;
+
+    std::vector<cv::DMatch > matches;
+
+    int movementDirection = 0;
+    std::string image_path1 = cv::samples::findFile("Hill1.jpg");
+    std::string image_path2 = cv::samples::findFile("Hill2.jpg");
+
+    cv::Mat image1 = cv::imread(image_path1);
+    cv::Mat image2 = cv::imread(image_path2);
+
+  
+
 }
